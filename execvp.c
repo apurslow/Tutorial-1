b67ec6f46993f62b0411ec9ba10292af16060342
Aaron <aaron26@uw.edu>
2023-12-13 18:07:55 -0800

    forgot last position in string array. Fixed to null.

--- execvp.c
+++ execvp.c
@@ -20,10 +20,11 @@ char ** args = malloc( ARGCNT * sizeof(char*));
 *(args + 1) = arg1;
 *(args + 2) = arg2;
 *(args + 3) = file;
+*(args + 4) = NULL;
 printf("cmd->");
 fscanf(stdin, "%s", cmd);
 printf("char %lu=%d\n",strlen(cmd),cmd[strlen(cmd)]);
-
+for (int i=0;i<ARGCNT;i++) printf("i=%d args[i]=%s\n",i,*(args + i));
 
 
 int execvp(const char *file, char *const argv[]);
